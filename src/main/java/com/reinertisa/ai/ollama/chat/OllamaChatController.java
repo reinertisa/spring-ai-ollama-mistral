f1497d0f260678b411af0b893c6100be260f63ea
Isa Reinert <reinert.isa@gmail.com>
2025-12-26 17:49:00 -0800

    feat(config): create ollama chat controller

---
+++ src/main/java/com/reinertisa/ai/ollama/chat/OllamaChatController.java
@@ -0,0 +1,36 @@
+package com.reinertisa.ai.ollama.chat;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api/v1")
+public class OllamaChatController {
+    private final ChatClient chatClient;
+
+    public OllamaChatController(@Qualifier("ollamaChatClient") ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/chat")
+    public String chat() {
+        return chatClient.prompt()
+                .user("Tell me an interesting fact about Java")
+                .call()
+                .content();
+    }
+
+    @GetMapping("/stream")
+    public Flux<String> stream() {
+        return chatClient.prompt()
+                .user("I'm visiting Hilton Head soon, can you give me 10 places I must visit?")
+                .stream()
+                .content();
+    }
+}
