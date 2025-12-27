9e001be6bd185b495b3a5bf2f442485cf958297f
Isa Reinert <reinert.isa@gmail.com>
2025-12-26 17:48:27 -0800

    feat(api): create ollama chat client bean

---
+++ src/main/java/com/reinertisa/ai/ollama/Application.java
@@ -0,0 +1,21 @@
+package com.reinertisa.ai.ollama;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ChatClient ollamaChatClient(OllamaChatModel ollamaChatModel) {
+        return ChatClient.create(ollamaChatModel);
+    }
+
+}
