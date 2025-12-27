8f8e1b29e9bf16590365c2e4bf3059f82cebc919
Isa Reinert <reinert.isa@gmail.com>
2025-12-26 16:38:11 -0800

    feat(api): initialize project

---
+++ src/main/java/com/reinertisa/ai/ollama/SpringAiOllamaMistralApplication.java
@@ -0,0 +1,13 @@
+package com.reinertisa.ai.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiOllamaMistralApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiOllamaMistralApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/reinertisa/ai/ollama/SpringAiOllamaMistralApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reinertisa.ai.ollama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiOllamaMistralApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
