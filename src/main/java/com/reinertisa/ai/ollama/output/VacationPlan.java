f687ac3dc48b17b800d50347da50d8b6a0487307
Isa Reinert <reinert.isa@gmail.com>
2025-12-27 22:16:55 -0800

    feat(ai): add unstructured output for vacation plan

---
+++ src/main/java/com/reinertisa/ai/ollama/output/VacationPlan.java
@@ -0,0 +1,24 @@
+package com.reinertisa.ai.ollama.output;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class VacationPlan {
+
+    ChatClient chatClient;
+    public VacationPlan(ChatClient chatClient) {
+        this.chatClient = chatClient;
+    }
+
+    @GetMapping("/vacation/unstructured")
+    public String unstructured() {
+        return chatClient.prompt()
+                .user("I want to plan a trip to Hawaii. Give me a list of things to do.")
+                .call()
+                .content();
+    }
+}
